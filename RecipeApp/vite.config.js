e8433cdf5129dd814569a664444fb3194837c6af
BobchopGaming <jmomolon@gmail.com>
2024-11-08 19:17:18 +0100

    Update vite.config.js

--- RecipeApp/vite.config.js
+++ RecipeApp/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react-icons/md', 'react-icons/fa', 'react-icons/io5'],  // Include io5 icon set
+      external: ['react-icons/md', 'react-icons/fa', 'react-icons/io5', 'react-spinners'],
     },
   },
 });
